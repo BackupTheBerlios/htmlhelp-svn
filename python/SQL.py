71b15db0a291f72f9a68c18b675ac76ff42471a8
jrfonseca <jrfonseca@0ff8d260-08be-0310-aec6-8af4c65d4e90>
2003-10-20 16:58:59 +0000

    Dump a book into a SQL db (almost complete).

--- python/SQL.py
+++ python/SQL.py
@@ -1,4 +1,4 @@
-
+"""SQL database."""
 
 if 0:
 	import MySQLdb
@@ -13,76 +13,95 @@ if 0:
 			
 	connection.close()
 
-"""HTML based used interface."""
-
-
 import os.path, posixpath, mimetypes
 
 import Book
 
 
 def guess_type(path):
-        base, ext = posixpath.splitext(path)
+	base, ext = posixpath.splitext(path)
 	if ext in mimetypes.types_map:
 		return mimetypes.types_map[ext]
 	else:
 		return 'application/octet-stream'
 
-def export(book, connection = None):
 
-	print "INSERT INTO books (name, toc_id, index_id) VALUES (%s, %s, %s);", (name.encode('utf-8'), toc_id, index_id)
-	book_id = 1
+def quote(str):
+	return repr(str)
 
-	name = 'test'
-	toc_id = export_contents(connection, book.contents, 1)
-	#index_id = export_contents(connection, book.index, 1)
-	index_id = 0
+#######################################################################
+# Dump a book into a SQL language file
+
+
+def dump(book):
+	dump_book(book)
+
+
+def dump_book(book):
+	print "INSERT INTO `books` (`title`, `default_link`) VALUES (%s, %s);" % (quote(book.title.encode('utf-8')), quote(book.default_link.encode('utf-8')))
+	print "SET @book_id = LAST_INSERT_ID();" 
+
+	dump_contents(book.contents)
+	#dump_index(book.index, 0)
+	dump_archive(book.archive)
 	
-	export_archive(connection, book.archive, book_id)
-	
 
-def export_contents(connection, entry, parent_id):
-
-	print "INSERT INTO toc (parent_id, number, name, link) VALUES (%s, %s, %s)", (parent_id, entry.name.encode('utf-8'), entry.link.encode('utf-8'))
+def dump_contents(contents):
+	print "INSERT INTO `toc` (`book_id`, `parent_id`, `number`, `name`, `link`) VALUES"
+	parent_id = 0
 	id = parent_id + 1
+	for entry in contents:
+		id = dump_contents_entry(entry, id, parent_id)
+	print ";"
+
+	print "UPDATE `toc` SET `parent_id` = LAST_INSERT_ID() + `parent_id` - 1 WHERE `book_id` = @book_id AND `parent_id` != 1;"
 	
-	for child in entry.children:
-		number += 1
-		export_contents(connection, child, id)
+	
+def dump_contents_entry(entry, id, parent_id):
+	print "  (@book_id, %d, %d, %s, %s)," % (parent_id, entry.number, quote(entry.name.encode('utf-8')), quote(entry.link.encode('utf-8')))
+	parent_id = id
+	id = parent_id + 1
+	for subentry in entry:
+		id = dump_contents_entry(subentry, id, parent_id)
 	
 	return id
 	
-def export_index(connection, entry, parent_id):
 
-	print "INSERT INTO index (parent_id, name) VALUES (%s, %s)", (parent_id, entry.name.encode('utf-8'))
+def dump_index_entry(entry, id, parent_id):
+
+	print "INSERT INTO `index` (`parent_id`, `name`) VALUES (%s, %s)" % (parent_id, entry.name.encode('utf-8'))
 	id = parent_id + 1
 
 	for link in entry.links:
-		print "INSERT INTO index_links VALUES (%s, %s)", (id, link.encode('utf-8'))
+		print "INSERT INTO `index_links` VALUES (%s, %s)", (id, link.encode('utf-8'))
 		
-	for child in entry.children:
-		export_index(connection, child, id)
+	parent_id = id
+	id = parent_id + 1
+	for subentry in entry:
+		id = dump_index_entry(child, id, parent_id)
 	
 	return id
 
-def export_archive(connection, archive, book_id):
 
-	for path in archive.list:
+def dump_archive(archive):
+	print "INSERT INTO `pages` (book_id, path, data) VALUES"
+	for path in archive.list():
 		data = archive.open(path).read()
-		print "INSERT INTO pages (book_id, path, data) VALUES (%s, %s, %s)", (book_id, path.encode('utf-8'), "...")
+		print "  (@book_id, %s, %s)," % (quote(path.encode('utf-8')), quote(data))
+	print ";"
 
 
 def main():
 	import sys
-	import HTB
+	from DevHelp import factory
 	
 	for arg in sys.argv[1:]:
 		try:
-			book = HTB.factory(arg)
+			book = factory(arg)
 		except:
 			raise
 
-		export(book)
+		dump(book)
 	
 
 
