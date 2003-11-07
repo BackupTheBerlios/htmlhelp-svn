bd06e9a7a5fbc29bae9bc882c9d27921158cc48f
jrfonseca <jrfonseca@0ff8d260-08be-0310-aec6-8af4c65d4e90>
2003-11-07 23:13:33 +0000

    Reorganized the source into a proper package.

---
+++ python/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: iso8859-1 -*-
+
+from distutils.core import setup, Extension
+ 
+chmlib = Extension(
+	'_chmlib', 
+	sources = ['chmlib/chmlib.i'],
+	libraries = ['chm'])
+
+setup(
+	name = "HTMLHelp",
+	version = "0.1",
+	description = 'HTML Help Python API',
+	author = 'José Fonseca',
+	author_email = 'jrfonseca@users.berlios.de',
+	url = 'http://htmlhelp.berlios.de/',
+	package_dir = {'': 'chmlib', 'HTMLHelp': 'HTMLHelp'},
+	packages = ['HTMLHelp'],
+	py_modules = ['chmlib'],
+	ext_modules = [chmlib])
