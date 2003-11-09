c674b77f59f61501526895affe9a0e8762b62ad7
jrfonseca <jrfonseca@0ff8d260-08be-0310-aec6-8af4c65d4e90>
2003-11-09 17:14:08 +0000

    Drop the idea that the index is a tree - it's not pratical.

--- www/php/_index.php
+++ www/php/_index.php
@@ -7,24 +7,18 @@
 
 	echo '<body id="index" class="sidebar">';
 
-	function walk_index($book_id, $parent_id)
+	if($book_id = intval($_GET['book_id']))
 	{
-		$result = mysql_query('SELECT `book_id`, `id`, `parent_id`, `term` FROM `index` WHERE `book_id`=' . $book_id . ' AND `parent_id`=' . $parent_id . ' ORDER BY `term`');
+		$result = mysql_query('SELECT `id`, `book_id`, `term` FROM `index` WHERE `book_id`=' . $book_id . ' ORDER BY `term`');
 		echo '<ul>';
-		while(list($book_id, $id, $parent_id, $term) = mysql_fetch_row($result))
+		while(list($index_id, $book_id, $term) = mysql_fetch_row($result))
 		{
-			list($index_id, $path, $anchor) = mysql_fetch_row(mysql_query('SELECT `index_id`, `path`, `anchor` FROM `index_links` WHERE `index_id`=' . $id));
+			list($index_id, $path, $anchor) = mysql_fetch_row(mysql_query('SELECT `index_id`, `path`, `anchor` FROM `index_links` WHERE `index_id`=' . $index_id));
 			
-			echo '<li>';
-			echo '<a href="page.php/' . $book_id . '/' . $path . '#' . $anchor . '" target="main">' . $term . '</a>';
-			walk_index($book_id, $id);
-			echo '</li>';
+			echo '<li><a href="page.php/' . $book_id . '/' . $path . '#' . $anchor . '" target="main">' . $term . '</a></li>';
 		}
 		echo '</ul>';
 	}
-	
-	if($book_id = intval($_GET['book_id']))
-		walk_index($book_id, 0);
 	
 	echo '</body>';
 	
