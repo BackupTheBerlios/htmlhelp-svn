8132c5f918cac2f6defbbca7be03ea0cd7a8df24
jrfonseca <jrfonseca@0ff8d260-08be-0310-aec6-8af4c65d4e90>
2004-01-17 22:16:48 +0000

    First mockup of a XUL enabled PHP frontend!

---
+++ www/php/main.js
@@ -0,0 +1,19 @@
+function doSelect(event)
+{
+  // the tree is the target of the event
+  var tree=event.target;
+
+  // get the label element
+  var label=document.getElementById("body");
+
+  // get the text of the description column in the selected row.
+  // First, call the getCellText function of the tree's view.
+  // We need to supply two parameters, the selected row index
+  // held in the tree's currentIndex property and the id of the
+  // column.
+  var txt=tree.view.getCellText(tree.currentIndex,"link")
+
+  // assign the text to the label.
+  label.setAttribute("src",txt);
+}
+
