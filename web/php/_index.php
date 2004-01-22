9d64c1cf09ba503a571e9d2d7aaaf978349fbb45
jrfonseca <jrfonseca@0ff8d260-08be-0310-aec6-8af4c65d4e90>
2004-01-22 00:43:16 +0000

    Add an index filter to the plain HTML interface.

--- web/php/_index.php
+++ web/php/_index.php
@@ -9,18 +9,24 @@
 	$target = 'main';
 	include 'header.inc.php';
 
+	$book_id = intval($_GET['book_id']);
+	$query = $_GET['query'];
+	
 	echo '<body id="index" class="sidebar">';
 
-	if($book_id = intval($_GET['book_id']))
+	echo '<form class="search" target="navigation" action="_index.php">';
+	echo  '<input type="hidden" name="book_id" value="' . $book_id .'"/>';
+	echo  '<input type="text" name="query" value=""/>';
+	echo  '<input type="submit" value="Find">';
+	echo '</form>';
+
+	$result = mysql_query('SELECT `term`, `path`, `anchor` FROM `index_entry`,`index_link` WHERE `index_entry`.`book_id`=' . $book_id . ' AND `index_link`.`book_id`=' . $book_id . ' AND `index_link`.`no`=`index_entry`.`no`' . ($query ? ' AND LOCATE(\'' . mysql_escape_string($query) . '\', `term`)' : '') . ' ORDER BY `index_entry`.`term`');
+	if(mysql_num_rows($result))
 	{
-		$result = mysql_query('SELECT `term`, `path`, `anchor` FROM `index_entry`,`index_link` WHERE `index_entry`.`book_id`=' . $book_id . ' AND `index_link`.`book_id`=' . $book_id . ' AND `index_link`.`no`=`index_entry`.`no` ORDER BY `index_entry`.`term`');
-		if(mysql_num_rows($result))
-		{
-			echo '<ul>';
-			while(list($term, $path, $anchor) = mysql_fetch_row($result))
-				echo '<li><a href="page.php/' . $book_id . '/' . $path . ($anchor ? '#' . $anchor : '') . '">' . $term . '</a></li>';
-			echo '</ul>';
-		}
+		echo '<ul>';
+		while(list($term, $path, $anchor) = mysql_fetch_row($result))
+			echo '<li><a href="page.php/' . $book_id . '/' . $path . ($anchor ? '#' . $anchor : '') . '">' . $term . '</a></li>';
+		echo '</ul>';
 	}
 	
 	echo '</body>';
