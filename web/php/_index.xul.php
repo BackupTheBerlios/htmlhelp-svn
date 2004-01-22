3a7d8c11eb5a3d8e22a84c6a1aeb5911107d0840
jrfonseca <jrfonseca@0ff8d260-08be-0310-aec6-8af4c65d4e90>
2004-01-22 14:28:09 +0000

    Only generate the index if a query is specified.

--- web/php/_index.xul.php
+++ web/php/_index.xul.php
@@ -21,10 +21,13 @@
 	echo '<textbox id="query" type="autocomplete" value="' . htmlspecialchars($query) . '" onkeypress="onQueryKeypress(event, ' . $book_id . ')"/>';
 	
 	echo '<listbox seltype="single" flex="1" onselect="onIndexSelect(event, ' . $book_id . ')">';
-	$result = mysql_query('SELECT `term`, `path`, `anchor` FROM `index_entry`,`index_link` WHERE `index_entry`.`book_id`=' . $book_id . ' AND `index_link`.`book_id`=' . $book_id . ' AND `index_link`.`no`=`index_entry`.`no`' . ($query ? ' AND LOCATE(\'' . mysql_escape_string($query) . '\', `term`)' : '') . ' ORDER BY `index_entry`.`term`');
-	while(list($term, $path, $anchor) = mysql_fetch_row($result))
+	if(isset($query))
 	{
-		echo '<listitem label="' . htmlspecialchars($term) . '" value="' . $path . ($anchor ? '#' . $anchor : '') . '"/>';
+		$result = mysql_query('SELECT `term`, `path`, `anchor` FROM `index_entry`,`index_link` WHERE `index_entry`.`book_id`=' . $book_id . ' AND `index_link`.`book_id`=' . $book_id . ' AND `index_link`.`no`=`index_entry`.`no`' . ($query ? ' AND LOCATE(\'' . mysql_escape_string($query) . '\', `term`)' : '') . ' ORDER BY `index_entry`.`term`');
+		while(list($term, $path, $anchor) = mysql_fetch_row($result))
+		{
+			echo '<listitem label="' . htmlspecialchars($term) . '" value="' . $path . ($anchor ? '#' . $anchor : '') . '"/>';
+		}
 	}
 	echo '</listbox>';
 
