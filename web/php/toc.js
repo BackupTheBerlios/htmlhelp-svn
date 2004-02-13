c81675469594c068f1ff58888d2f5d16e86ea85e
jrfonseca <jrfonseca@0ff8d260-08be-0310-aec6-8af4c65d4e90>
2004-02-13 13:31:50 +0000

    Proper TOC syncing.

--- web/php/toc.js
+++ web/php/toc.js
@@ -1,35 +1,67 @@
 var iframe = parent.frames['body'];
-  
+
+var noSelect = 0;
+
 function onTocSelect(event, book_id)
 {
-  var tree = event.target;
-  var link = tree.view.getCellValue(tree.currentIndex, "name");
+	var tree = event.target;
+	var link = tree.view.getCellValue(tree.currentIndex, "name");
 
-  iframe.location.href = "page.php/" + book_id + "/" + link;
+	if(!noSelect)
+		iframe.location.href = "page.php/" + book_id + "/" + link;
+}
+
+function parentTreeItem(element)
+{
+	do
+		element = element.parentNode;
+	while(element.nodeName != 'treeitem')
+
+	return element;
+}
+
+function showTreeItem(tree, item)
+{
+	if(tree.contentView.getIndexOfItem(item) == -1)
+	{
+		var parentItem = parentTreeItem(item);
+		showTreeItem(tree, parentItem);
+		parentItem.setAttribute('open', 'true');
+	}
+}
+
+function selectTreeItem(tree, item)
+{
+	noSelect = 1;
+	showTreeItem(tree, item);
+	var row = tree.contentView.getIndexOfItem(item);
+	tree.view.selection.select(row);
+	tree.currentIndex = row;
+	noSelect = 0;
+	tree.treeBoxObject.ensureRowIsVisible(row);
 }
 
 function onButtonCommand(event)
 {
-  var tree = document.getElementById("tree");
+	var tree = document.getElementById("tree");
 
-  var href = parent.body.location.href;
-  var base = document.location.href.replace(/\/toc\.xul\.php\?book_id=(.*)$/,'/page.php/$1/')
+	var href = parent.body.location.href;
+	var base = document.location.href.replace(/\/toc\.xul\.php\?book_id=(.*)$/,'/page.php/$1/')
 
-  if(href.substr(0, base.length) == base)
-  {
-    var link = href.substr(base.length);
-    
-    // FIXME: this only searches the visible rows
-    for(row = 0; row < tree.view.rowCount; row++)
-    {
-    	if(tree.view.getCellValue(row, "name") == link)
+	if(href.substr(0, base.length) == base)
 	{
-	  tree.view.selection.select(row);
-	  return;
+		var link = href.substr(base.length);
+		var elements = tree.contentView.root.getElementsByTagName('treecell');
+
+		for(var i = 0; i < elements.length; ++i)
+		{
+			if(elements[i].getAttribute('value') == link)
+			{
+				var item = parentTreeItem(elements[i]);
+				selectTreeItem(tree, item);
+			}
+		}
 	}
-    }
-    alert("Could not find topic for current page.");
-  }
 }
 
 // FIXME: eventually make the sync automatic
