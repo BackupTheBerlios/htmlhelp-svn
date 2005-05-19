ce64be3400a113c646e884827de8ea3b1fc4bae9
jrfonseca <jrfonseca@0ff8d260-08be-0310-aec6-8af4c65d4e90>
2005-05-19 00:03:23 +0000

    Cobertura dos testes.

--- python/test/runtests.py
+++ python/test/runtests.py
@@ -6,11 +6,11 @@ import unittest
 
 
 def main():
-		suite = unittest.TestSuite()
-		for name in os.listdir('.'):
-			if name.startswith('test_') and name.endswith('.py'):
-				suite.addTest(unittest.defaultTestLoader.loadTestsFromName(name[:-3]))
-		unittest.TextTestRunner(verbosity=2).run(suite)
+	suite = unittest.TestSuite()
+	for name in os.listdir('.'):
+		if name.startswith('test_') and name.endswith('.py'):
+			suite.addTest(unittest.defaultTestLoader.loadTestsFromName(name[:-3]))
+	unittest.TextTestRunner(verbosity=2).run(suite)
 
 
 if __name__ == '__main__':
---
+++ python/test/runtestscov.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+
+import os
+import os.path
+import unittest
+import sys
+from coverage import the_coverage as coverage
+
+dir = os.path.abspath('../htmlhelp/')
+
+def main():
+	coverage.erase()
+	suite = unittest.TestSuite()
+	
+	for name in os.listdir('.'):
+		if name.startswith('test_') and name.endswith('.py'):
+			suite.addTest(unittest.defaultTestLoader.loadTestsFromName(name[:-3]))
+	coverage.start()
+	unittest.TextTestRunner(stream=sys.stdout, verbosity=2).run(suite)
+	coverage.stop()
+
+	print
+	
+	global dir
+	modules = [name for name in coverage.cexecuted.iterkeys() if name.startswith(dir)]
+	modules.sort()
+	coverage.report(modules)
+
+
+if __name__ == '__main__':
+	main()
