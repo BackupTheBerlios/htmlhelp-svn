8f67deb3e3f74fef9715be83d6684788d839f42f
jrfonseca <jrfonseca@0ff8d260-08be-0310-aec6-8af4c65d4e90>
2005-05-25 16:00:03 +0000

    Do not cache coverage data.

--- python/test/runtestscov.py
+++ python/test/runtestscov.py
@@ -10,6 +10,7 @@ from coverage import the_coverage as coverage
 dir = os.path.abspath('../htmlhelp/')
 
 def main():
+	coverage.use_cache(False)
 	coverage.erase()
 	suite = unittest.TestSuite()
 	
@@ -23,6 +24,7 @@ def main():
 	print
 	
 	global dir
+	coverage.canonicalize_filenames()
 	modules = [name for name in coverage.cexecuted.iterkeys() if name.startswith(dir)]
 	modules.sort()
 	coverage.report(modules)
