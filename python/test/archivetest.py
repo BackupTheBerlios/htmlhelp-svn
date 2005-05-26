5ee6d8b0377955284dd4842e6e7944d70d3c54b6
jrfonseca <jrfonseca@0ff8d260-08be-0310-aec6-8af4c65d4e90>
2005-05-26 14:01:26 +0000

    More verbose failure messages.

--- python/test/archivetest.py
+++ python/test/archivetest.py
@@ -8,31 +8,38 @@ class ArchiveTestCase(unittest.TestCase):
 
 	missing_path = 'AVeryUnlikelyFileName'
 	
-	def failUnlessFileObj(self, fp):
+	def failUnlessFileObj(self, fp, path):
+		"""Fail if fp is not a read file-like object.
+		
+		Given Python dynamic type system it is not enough to verify if fp is an
+		instance of 'file'. Instead we verify if all the usual file reading methods
+		exist."""
+		
 		for method in [
 				'close',
-				'flush',
 				'read',
 				'readline',
+				'seek',
 				'tell']:
-			self.failUnless(hasattr(fp, method))
-			self.failUnless(callable(getattr(fp, method)))
+			self.failUnless(hasattr(fp, method), "'%s' method is missing from '%s' file" % (method, path))
+			self.failUnless(callable(getattr(fp, method)), "'%s' method is not callable '%s' file" % (method, path))
 		
 	def testContains(self):
 		for path in self.archive:
-			self.failUnless(path in self.archive)
-		self.failIf(self.missing_path in self.archive)
+			self.failUnless(path in self.archive, "'%s' is listed in the archive but reported as missing" % path)
+		self.failIf(self.missing_path in self.archive, "'%s' should be reported as missing" % self.missing_path)
 
 	def testGetItem(self):
 		for path in self.archive:
 			try:
 				fp = self.archive[path]
 			except KeyError:
-				self.fail()
+				self.fail("'%s' is listed in the archive but not accessible" % path)
+			self.failUnlessFileObj(fp, path)
 				
 		try:
 			fp = self.archive[self.missing_path]
-			self.fail()
+			self.fail("'%s' should not be accessible" % self.missing_path)
 		except KeyError:
 			pass
 	
@@ -45,13 +52,18 @@ class ArchiveTestCase(unittest.TestCase):
 
 class SampleArchiveTestCase(ArchiveTestCase):
 
+	paths = [
+		'empty',
+		'file',
+		'dir/file',
+		'dir/subdir/file']
+	
 	def testList(self):
-		for path in [
-				'empty',
-				'file',
-				'dir/file',
-				'dir/subdir/file']:
-			self.failUnless(path in self.archive)
+		for path in self.paths:
+			self.failUnless(path in self.archive, "'%s' is missing" % path)
+			
+	def testLen2(self):
+		self.failUnlessEqual(len(self.archive), len(self.paths))
 			
 	def testEmpty(self):
 		fp = self.archive['empty']
@@ -63,4 +75,4 @@ class SampleArchiveTestCase(ArchiveTestCase):
 				'dir/file',
 				'dir/subdir/file']:
 			fp = self.archive['dir/subdir/file']
-			self.failUnlessEqual(fp.read(), 'Test\n')
+			self.failUnlessEqual(fp.read(), 'Sample...\n')
