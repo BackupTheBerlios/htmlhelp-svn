a511ffdc13c5ae7980a2a29c75ea2b93687405d8
jrfonseca <jrfonseca@0ff8d260-08be-0310-aec6-8af4c65d4e90>
2005-08-11 23:32:55 +0000

    Bootstrapped a PHP fulltext indexer.

---
+++ web/php/indexer.inc.php
@@ -0,0 +1,116 @@
+<?php
+
+require_once 'config.inc.php';
+require_once 'mysql.inc.php';
+require_once 'mimetypes.inc.php';
+
+class Index
+{
+	function Index()
+	{
+	}
+
+	function store_title($page, $title) {}
+
+	function store_lexemes($page, $lexemes) {}
+
+	// Indexer Factory Method
+	function indexer($page)
+	{
+		$content_type = guess_type($page);
+		if($content_type == "text/plain")
+			return new TextIndexer($this, $page);
+		elseif($content_type == "text/html")
+			return new HtmlIndexer($this, $page);
+		return NULL;
+	}
+
+	function index_($page, $content)
+	{
+		$indexer = $this->indexer($page);
+		if(!is_null($indexer))
+			$indexer->feed($content);
+	}
+}
+
+class Indexer
+{
+	var $index;
+	var $page;
+
+	function Indexer($index, $page)
+	{
+		$this->index = $index;
+		$this->page = $page;
+	}
+
+	function feed_title($title)
+	{
+		$this->index->store_title($this->page, $this->normalize($title));
+	}
+
+	function feed_body($body)
+	{
+		$this->index->store_lexemes($this->page, $this->tokenize($body));
+	}
+
+	function feed($content) {}
+
+	// Normalize whitespace
+	function normalize($str)
+	{
+		return implode(' ', preg_split('/\s/', $str, -1, PREG_SPLIT_NO_EMPTY));
+	}
+
+	function tokenize($str)
+	{
+		$lexemes = preg_split('/\s/', $s, -1, PREG_SPLIT_NO_EMPTY);
+		return $lexemes;
+	}
+}
+
+class TextIndexer extends Indexer
+{
+	function feed($content)
+	{
+		$this->feed_body($content);
+	}
+}
+
+class HtmlIndexer extends Indexer
+{
+	function feed($content)
+	{
+		$title = $this->extract_title($html);
+		if($title)
+			$this->feed_title($title);
+
+		$body = $this->extract_body($html);
+		if($body)
+			$this->feed_body($body);
+	}
+
+	function extract_encoding($html)
+	{
+		// FIXME: determine HTML encoding
+		return "ISO8859-1";
+	}
+
+	function extract_title($html)
+	{
+		if(preg_match("/<title(?:\s.*?)?>(.*?)<\/title\s*>/is", $html, $matches))
+			return html_entity_decode($matches[1]);
+		else
+			return NULL;
+	}
+
+	function extract_body($html)
+	{
+		if(preg_match("/<body(?:\s.*?)?>(.*?)<\/body\s*>/is", $html, $matches))
+			return html_entity_decode(preg_replace("/<.*?>/s", "", $matches[1]));
+		else
+			return NULL;
+	}
+}
+
+?>
---
+++ web/php/test_indexer.php
@@ -0,0 +1,116 @@
+<?php
+
+require_once 'indexer.inc.php';
+require_once 'PHPUnit.php';
+
+class IndexTest extends PHPUnit_TestCase
+{
+	var $index;
+
+	function setUp()
+	{
+		$this->index = new Index();
+	}
+
+	function tearDown()
+	{
+		unset($this->index);
+	}
+
+	function testIndexer()
+	{
+		$testcases = array(
+			'abc.txt' => 'TextIndexer',
+			'abc.htm' => 'HtmlIndexer',
+			'abc.html' => 'HtmlIndexer',
+		);
+		foreach($testcases as $path => $class)
+			$this->assertTrue(is_a($this->index->indexer($path), $class), $class);
+	}
+}
+
+class IndexStub extends Index
+{
+	var $title;
+	var $lexemes;
+
+	function IndexStub()
+	{
+		$this->reset();
+	}
+	
+	function reset()
+	{
+		$this->title = NULL;
+		$this->lexemes = array();
+	}
+
+	function store_title($page, $title)
+	{
+		$this->title = $title;
+	}
+
+	function store_lexemes($page, $lexemes)
+	{
+		foreach($lexemes as $lexeme)
+			$this->lexemes[$lexeme] += 1;
+	}
+}
+
+class IndexerTest extends PHPUnit_TestCase
+{
+	function testNormalize()
+	{
+		$testcases = array(
+			"" => "",
+			" \n\t " => "",
+			"  a house" => "a house",
+			" a   house " => "a house",
+			"a  house" => "a house",
+		);
+		foreach($testcases as $string => $result)
+			$this->assertEquals($result, TextIndexer::normalize($string));
+	}
+}
+
+class HtmlIndexerTest extends PHPUnit_TestCase
+{
+	function testExtractTitle()
+	{
+		$testcases = array(
+			"before<title></title>after" => "",
+			"<title>Simple</title>" => "Simple",
+			"<Title>Ignore case</TITLE>" => "Ignore case",
+			"<title id=\"id123\">Attributes</title>" => "Attributes",
+			"<title id=\"id123\">Html entities: &amp;&lt;&gt;</title >" => "Html entities: &<>",
+			"<title\n\n>\nNewlines\n</title\n\n>" => "\nNewlines\n",
+		);
+		foreach($testcases as $html => $title)
+			$this->assertEquals($title, HtmlIndexer::extract_title($html));
+	}
+
+	function testExtractBody()
+	{
+		$testcases = array(
+			"before<body></body>after" => "",
+			"<body>Simple</body>" => "Simple",
+			"<Body>Ignore case</BODY>" => "Ignore case",
+			"<body id=\"id123\">Attributes</body>" => "Attributes",
+			"<body id=\"id123\">Html entities: &amp;&lt;&gt;</body >" => "Html entities: &<>",
+			"<body><b class=\"bold\">Tags</b></body\n\n>" => "Tags",
+			"<body\n\n>\nNewlines\n</body\n\n>" => "\nNewlines\n",
+		);
+		foreach($testcases as $html => $body)
+			$this->assertEquals($body, HtmlIndexer::extract_body($html));
+	}
+}
+
+$cases = array(
+	'IndexTest',
+	'IndexerTest',
+	'HtmlIndexerTest',
+);
+foreach($cases as $case)
+	echo PHPUnit::run(new PHPUnit_TestSuite($case))->toString();
+
+?>
