27e8e01bb17b3a8861a3cd830fb5a8196d0f35fb
jrfonseca <jrfonseca@0ff8d260-08be-0310-aec6-8af4c65d4e90>
2005-09-30 10:58:01 +0000

    Cache lexemes in a temporary SQL table.

--- web/php/include/book_fulltext_index.inc.php
+++ web/php/include/book_fulltext_index.inc.php
@@ -22,45 +22,9 @@ class Book_Fulltext_Index extends Fulltext_Index
 		mysql_query("DELETE FROM lexeme_link WHERE book_id = $this->book_id");
 		mysql_query("DELETE FROM lexeme WHERE book_id = $this->book_id");
 		mysql_query("UPDATE page SET title='' WHERE book_id = $this->book_id");
-	}
 
-	function cleanup()
-	{
-		// drop lexemes which start with a digit and appear only once
-		mysql_query("
-			CREATE TEMPORARY TABLE delete_lexeme
-				SELECT lexeme 
-					FROM lexeme
-						LEFT JOIN lexeme_link ON lexeme.no = lexeme_link.no
-					WHERE lexeme.book_id = $this->book_id
-						AND lexeme >= '0' AND lexeme < 'A'
-						AND lexeme_link.book_id = $this->book_id
-					GROUP BY lexeme.book_id, lexeme
-					HAVING SUM(count) = 1
-		") or die(__FILE__ . ':' . __LINE__ . ':' . mysql_error());
-		mysql_query("
-			DELETE lexeme, lexeme_link
-			FROM lexeme, delete_lexeme
-				LEFT JOIN lexeme_link ON lexeme.no = lexeme_link.no
-			WHERE lexeme.book_id = $this->book_id
-				AND lexeme.lexeme IN (delete_lexeme.lexeme)
-				AND lexeme_link.book_id = $this->book_id
-		") or die(__FILE__ . ':' . __LINE__ . ':' . mysql_error());
-		mysql_query("
-			DROP TEMPORARY TABLE delete_lexeme 
-		") or die(__FILE__ . ':' . __LINE__ . ':' . mysql_error());
-		
-		// eliminate stop words from index
-		$result = mysql_query("
-			DELETE lexeme, lexeme_link
-			FROM stop_word
-				LEFT JOIN lexeme ON lexeme.lexeme = stop_word.lexeme
-				LEFT JOIN lexeme_link ON lexeme_link.no = lexeme.no
-			WHERE lexeme.book_id = $this->book_id
-				AND lexeme_link.book_id = $this->book_id
-		") or die(__FILE__ . ':' . __LINE__ . ':' . mysql_error());
 	}
-
+	
 	function set_page_no($page_no)
 	{
 		$this->page_no = $page_no;
@@ -70,7 +34,11 @@ class Book_Fulltext_Index extends Fulltext_Index
 	{
 		assert(isset($this->page_no));
 
-		$this->page_lexemes = array();
+		mysql_query("
+			CREATE TEMPORARY TABLE temp_lexeme (
+				lexeme varchar(31) NOT NULL
+			) TYPE = HEAP"
+		) or die(__FILE__ . ':' . __LINE__ . ':' . mysql_error() . "\n");
 	}
 
 	function set_title($title) 
@@ -91,45 +59,95 @@ class Book_Fulltext_Index extends Fulltext_Index
 
 	function add_lexemes(&$lexemes)
 	{
-		// TODO: store lexeme positions instead of lexeme counts
+		if(!count($lexemes))
+			return;
+		
+		$values = array();
 		foreach($lexemes as $lexeme)
-		{
-			$this->page_lexemes[$lexeme] += 1;
-		}
+			$values[] = '("' . mysql_escape_string($lexeme) . '")';		
+		mysql_query(
+			"INSERT INTO temp_lexeme " .
+			"(lexeme) " .
+			"VALUES " . implode(',', $values)
+		) or die(__FILE__ . ':' . __LINE__ . ':' . mysql_error() . "\n");
 	}
 
 	function finish_page()
 	{
-		foreach($this->page_lexemes as $lexeme => $count)
-		{
-			// get this lexeme number
-			$result = mysql_query("
-				SELECT no
-			 	FROM lexeme
-				WHERE book_id=$this->book_id AND lexeme='" . mysql_escape_string($lexeme) . "'
-			") or print(__FILE__ . ':' . __LINE__ . ':' . mysql_error() . "\n");
-			if(mysql_num_rows($result))
-				list($lexeme_no) = mysql_fetch_row($result);
-			else
-			{
-				$this->last_lexeme_no += 1;
-				$lexeme_no = $this->last_lexeme_no;
-				mysql_query(
-					"INSERT INTO lexeme 
-					(book_id, no, lexeme) VALUES 
-					($this->book_id, $lexeme_no, '" . mysql_escape_string($lexeme) . "')"
-				);
-			}
+		mysql_query(
+			"ALTER " .
+			"TABLE temp_lexeme " .
+			"ADD INDEX lexeme (lexeme(7))"
+		) or die(__FILE__ . ':' . __LINE__ . ':' . mysql_error());
+		
+		// eliminate stop words from index
+		mysql_query(
+			"DELETE temp_lexeme " .
+			"FROM stop_word " .
+			"LEFT JOIN temp_lexeme USING(lexeme)"
+		) or die(__FILE__ . ':' . __LINE__ . ':' . mysql_error());
 
-			mysql_query(
-				"INSERT INTO lexeme_link 
-				(book_id, no, page_no, count) VALUES 
-				($this->book_id, $lexeme_no, $this->page_no, $count)"
-			);
-		}
+		mysql_query(
+			"SELECT @lexeme_no := IFNULL(MAX(lexeme.no), 0) " .
+			"FROM lexeme " .
+			"WHERE book_id=$this->book_id" 
+		) or die(__FILE__ . ':' . __LINE__ . ':' . mysql_error() . "\n");
 
-		unset($this->page_lexemes);
+		mysql_query(
+			"INSERT " .
+			"INTO lexeme (book_id, no, lexeme) " .
+			"SELECT $this->book_id, @lexeme_no := (@lexeme_no + 1), temp_lexeme.lexeme " .
+			"FROM temp_lexeme " .
+			"LEFT JOIN lexeme ON lexeme.book_id = $this->book_id AND temp_lexeme.lexeme = lexeme.lexeme " .
+			"WHERE lexeme.no IS NULL " .
+			"GROUP BY temp_lexeme.lexeme"
+		) or die(__FILE__ . ':' . __LINE__ . ':' . mysql_error() . "\n");
+		
+		// TODO: store lexeme positions instead of lexeme counts
+		mysql_query(
+			"INSERT " .
+			"INTO lexeme_link (book_id, no, page_no, count) " .
+			"SELECT $this->book_id, no, $this->page_no, COUNT(*) " .
+			"FROM temp_lexeme " .
+			"LEFT JOIN lexeme ON temp_lexeme.lexeme = lexeme.lexeme " .
+			"WHERE lexeme.book_id = $this->book_id " .
+			"GROUP BY temp_lexeme.lexeme"
+		) or die(__FILE__ . ':' . __LINE__ . ':' . mysql_error() . "\n");
+
+		mysql_query(
+			"DROP TEMPORARY TABLE temp_lexeme" 
+		) or die(__FILE__ . ':' . __LINE__ . ':' . mysql_error());
+
 		unset($this->page_no);
+	}
+
+	function cleanup()
+	{
+		// drop lexemes which start with a digit and appear only once
+		mysql_query("
+			CREATE TEMPORARY TABLE delete_lexeme
+			SELECT lexeme 
+			FROM lexeme
+				LEFT JOIN lexeme_link ON lexeme.no = lexeme_link.no
+			WHERE lexeme.book_id = $this->book_id
+				AND lexeme >= '0' AND lexeme < 'A'
+				AND lexeme_link.book_id = $this->book_id
+			GROUP BY lexeme.book_id, lexeme
+			HAVING SUM(count) = 1
+		") or die(__FILE__ . ':' . __LINE__ . ':' . mysql_error());
+		
+		mysql_query("
+			DELETE lexeme, lexeme_link
+			FROM lexeme, delete_lexeme
+				LEFT JOIN lexeme_link ON lexeme.no = lexeme_link.no
+			WHERE lexeme.book_id = $this->book_id
+				AND lexeme.lexeme IN (delete_lexeme.lexeme)
+				AND lexeme_link.book_id = $this->book_id
+		") or die(__FILE__ . ':' . __LINE__ . ':' . mysql_error());
+		
+		mysql_query("
+			DROP TEMPORARY TABLE delete_lexeme 
+		") or die(__FILE__ . ':' . __LINE__ . ':' . mysql_error());
 	}
 }
 
--- web/php/include/mysql.inc.php
+++ web/php/include/mysql.inc.php
@@ -2,5 +2,6 @@
 
 mysql_connect($db_server, $db_username, $db_password);
 mysql_select_db($db_database);
+mysql_query('SET NAMES "utf8"');
 
 ?>
