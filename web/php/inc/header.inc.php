7292e5d7bf13a698f0f55412abc6d74f237cfcfc
jrfonseca <jrfonseca@0ff8d260-08be-0310-aec6-8af4c65d4e90>
2005-10-02 21:00:59 +0000

    Split index page into header plus footer parts.

---
+++ web/php/inc/header.inc.php
@@ -0,0 +1,45 @@
+<?php
+	require_once 'inc/config.inc.php';
+	require_once 'lib/book_catalog.lib.php';
+
+	header('Content-Type: text/html; charset=utf-8');
+
+	echo '<?xml version="1.0" encoding="UTF-8"?>';
+	echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
+	
+	echo '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">';
+	
+	echo '<head>';
+	echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>';
+	echo '<title>' . $title . '</title>';
+	echo '<link href="css/default.css" type="text/css" rel="stylesheet"/>';
+	echo '</head>';
+
+	echo '<body>';
+	
+	echo '<script src="js/index.js"></script>';
+
+	echo '<div class="header">' . $title . '</div>';
+	
+	$catalog = new BookCatalog();
+
+	echo '<div id="tags" class="sidebox">';
+	echo '<span class="title">Tags</span>';
+	echo '<table>';
+	$tags = $catalog->count_tags();
+	foreach($tags as $tag => $tag_count)
+	{
+		echo '<tr>';
+		echo  '<td>' . $tag_count . '</td>';
+		echo  '<td>';
+		echo   '<a href="index.php?tag=' . htmlspecialchars($tag) . '">';
+		echo    htmlspecialchars($tag, ENT_NOQUOTES);
+		echo   '</a>';
+		echo  '</td>';
+		echo '</tr>';
+	}
+	echo '</table>';
+	echo '</div>';
+	
+	echo '<div class="content">';
+?>
--- web/php/index.php
+++ web/php/index.php
@@ -1,47 +1,8 @@
 <?php
-	require_once 'inc/config.inc.php';
-	require_once 'lib/book_catalog.lib.php';
-
-	header('Content-Type: text/html; charset=utf-8');
-
-	echo '<?xml version="1.0" encoding="UTF-8"?>';
-	echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">';
+	$title = 'HTML Help Books';
 	
-	echo '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">';
+	require 'inc/header.inc.php';
 	
-	echo '<head>';
-	echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>';
-	echo '<title>HTML Help Books</title>';
-	echo '<link href="css/default.css" type="text/css" rel="stylesheet"/>';
-	echo '</head>';
-
-	echo '<body>';
-	
-	echo '<script src="js/index.js"></script>';
-
-	echo '<div class="header">HTML Help Books</div>';
-	
-	$catalog = new BookCatalog();
-
-	echo '<div id="tags" class="sidebox">';
-	echo '<span class="title">Tags</span>';
-	echo '<table>';
-	$tags = $catalog->count_tags();
-	foreach($tags as $tag => $tag_count)
-	{
-		echo '<tr>';
-		echo  '<td>' . $tag_count . '</td>';
-		echo  '<td>';
-		echo   '<a href="?tag=' . htmlspecialchars($tag) . '">';
-		echo    htmlspecialchars($tag, ENT_NOQUOTES);
-		echo   '</a>';
-		echo  '</td>';
-		echo '</tr>';
-	}
-	echo '</table>';
-	echo '</div>';
-	
-	echo '<div class="content">';
 	if($tag = $_GET['tag'])
 		$books = $catalog->enumerate_books_by_tag($tag);
 	else	
@@ -64,9 +25,6 @@
 	}
 	else
 		echo '<p>No book found.</p>';
-	echo '</div>';
 
-	echo '</body>';
-	
-	echo '</html>';
+	require 'inc/footer.inc.php';
 ?>
