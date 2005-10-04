6b0929594b47116c0d9c972930a7add09389ef02
jrfonseca <jrfonseca@0ff8d260-08be-0310-aec6-8af4c65d4e90>
2005-10-04 14:43:39 +0000

    Better HTML TOC tree.

--- web/php/tocfrm.php
+++ web/php/tocfrm.php
@@ -6,71 +6,57 @@ $id = 'toc';
 //$search_button = 'Filter';
 require 'inc/frmheader.inc.php';
 
-	function walk_children($entries, $depth)
+function walk_children($entries, &$linage)
+{
+	if(count($entries))
 	{
-		if(count($entries) && $depth)
-		{
-			echo '<ul class="tree">';
-			foreach($entries as $number => $entry)
-			{
-				list($name, $link, $children) = $entry;
-				walk_toc($number, $name, $link, $children, $depth - 1);
-			}
-			echo '</ul>';
-		}
-	}
-	
-	function walk_toc($number, $name, $link, $children, $depth)
-	{
-		global $book, $alias;
-		
-		$has_children = count($children);
-
-		if($has_children)
-		{
-			if($depth)
-				echo '<li class="expanded">';
-			else
-				echo '<li class="collapsed">';
-		}
-		else
-			echo '<li class="single">';
-		
-		if($depth || !$has_children)
-			echo '<a href="' . $link . '">';
-		else
-			echo '<a href="../../tocfrm.php?book=' . htmlspecialchars($alias) . '&amp;toc_no=' . $number . '" target="_self">';
-		echo htmlspecialchars($name, ENT_NOQUOTES) . '</a>';
-			
-		walk_children($children, $depth);
-		echo '</li>';
-	}
-
-	$number = intval($_GET['toc_no']);
-	$depth = 2;
-	
-	if($number)
-	{
-		list($parent_number, $title, $link) = $book->toc_entry($number);
-		
-		echo '<ul class="tree"><li class="collapsed"><a href="../../tocfrm.php?book=' . htmlspecialchars($alias) . '&amp;toc_no=' . $parent_number . '" target="_self">&hellip;</a>'; 
-			
 		echo '<ul class="tree">';
-		echo '<li class="expanded">';
-		if($link)
-			echo '<a href="' . htmlspecialchars($link) . '">' . htmlspecialchars($title, ENT_NOQUOTES) . '</a>';
-		else
-			echo htmlspecialchars($title, ENT_NOQUOTES);
-		
-		walk_children($book->toc($number, $depth), $depth - 1);
-		echo '</li>';
+		foreach($entries as $number => $entry)
+		{
+			list($name, $link, $children) = $entry;
+			walk_toc($number, $name, $link, $children, $linage);
+		}
 		echo '</ul>';
-		
-		echo '</li></ul>';
-		
+	}
+}
+
+function walk_toc($number, $name, $link, $children, &$linage)
+{
+	global $book, $alias, $toc_nos;
+	
+	$has_children = count($children);
+
+	if($has_children)
+	{
+		if($toc_nos[$number])
+			echo '<li class="expanded">';
+		else
+			echo '<li class="collapsed">';
 	}
 	else
-		walk_children($book->toc(0, $depth + 1), $depth);
+		echo '<li class="single">';
+
+	array_push($linage, $number);
+	
+	if($toc_nos[$number] || !$has_children)
+		echo '<a href="' . $link . '">';
+	else
+		echo '<a href="../../tocfrm.php?book=' . htmlspecialchars($alias) . '&amp;toc_nos=' . implode('+', $linage) . '" target="_self">';
+	echo htmlspecialchars($name, ENT_NOQUOTES) . '</a>';
+	
+	if($toc_nos[$number] && $has_children)
+		walk_children($children, $linage);
+	
+	array_pop($linage);
+	
+	echo '</li>';
+}
+
+$toc_nos = array();
+foreach(explode(' ', $_GET['toc_nos']) as $toc_no)
+	$toc_nos[intval($toc_no)] = TRUE;
+$linage = array();
+walk_children($book->toc(), $linage);
 	
 require 'inc/frmfooter.inc.php';
 ?>
