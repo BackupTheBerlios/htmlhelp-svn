26b12ffd717777f2b3f1ee372d31cfa35ac99191
jrfonseca <jrfonseca@0ff8d260-08be-0310-aec6-8af4c65d4e90>
2005-10-09 19:51:11 +0000

    Simplify code.

--- web/php/inc/mysql.inc.php
+++ web/php/inc/mysql.inc.php
@@ -1,34 +1,29 @@
 <?php
 
-$db_version_major = 1;
-$db_version_minor = 1;
+$db_version_major = 1; $db_version_minor = 1;
 
 mysql_connect($db_server, $db_username, $db_password);
 mysql_select_db($db_database);
 mysql_query("SET NAMES '$internal_encoding'");
 
-$result = mysql_query('SELECT major, minor FROM version');
-if($result && mysql_num_rows($result))
-	list($major, $minor) = mysql_fetch_row($result);
-else
-	list($major, $minor) = array(0, 0);
+$major = 0; $minor = 0;
+if(
+	($result = mysql_query('SELECT major, minor FROM version')) && 
+	mysql_num_rows($result) && 
+	(list($major, $minor) = mysql_fetch_row($result)) &&
+	($major == $db_version_major && $minor == $db_version_minor)
+)
+	return;
 
-if($major < $db_version_major)
+require_once('lib/mysql_util.lib.php');
+
+if($major != $db_version_major)
 {
-	require_once('lib/mysql_util.lib.php');
-	
 	mysql_import_dump('sql/create.sql');
-	mysql_import_dump('sql/tags.sql');
-	
-	mysql_query(
-		"INSERT " .
-		"INTO version (major, minor) " .
-		"VALUES ($db_version_major, $db_version_minor)"
-	) or die(__FILE__ . ':' . __LINE__ . ':' . mysql_error() . "\n");
+	mysql_import_dump('sql/tags.sql');	
 }
 elseif($minor < $db_version_minor)
 {
-	require_once('lib/mysql_util.lib.php');
 	mysql_import_dump('sql/update.sql');
 }
 
